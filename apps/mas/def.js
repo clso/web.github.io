897f8741a70f53a907550fa21abe4bf4e3af17ff
clso <leileiskzx@gmail.com>
2025-02-18 03:23:16 +0800

    update

---
+++ apps/mas/def.js
@@ -0,0 +1,17 @@
+﻿function getQueryParams(qs) {
+    //by http://stackoverflow.com/a/1099670
+    qs = qs.split('+').join(' ');
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+    while ((tokens = re.exec(qs))) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+    return params;
+}
+
+window.pars = getQueryParams(document.location.search);
+
+function exthas(name){
+	return typeof(window.external) !== 'undefined' && (name in window.external);
+}
